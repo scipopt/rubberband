401c9c8e66fdaa6c37989a906c4191855d89d694
Franziska Schloesser <schloesser@zib.de>
2017-08-31 18:59:47 +0200

    fix flake8

--- rubberband/utils/es_helpers.py
+++ rubberband/utils/es_helpers.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
 
+
 def get_uniques(model, field):
     '''
     Take an elasticsearch model and a field name. The looks up all of the possible values
@@ -12,8 +13,8 @@ def get_uniques(model, field):
     today = datetime.now()
     threemonthsago = (today + timedelta(days=-100)).strftime('%Y-%m-%d')
     # TODO make this work for the most recent testruns
-    body = {"filter": {"range": {"git_commit_timestamp": {"gte": threemonthsago }}},
-            "aggs": {"hot_counts": {"terms": {"field": field, "size": 5 }}}}
+    body = {"filter": {"range": {"git_commit_timestamp": {"gte": threemonthsago}}},
+            "aggs": {"hot_counts": {"terms": {"field": field, "size": 5}}}}
 
     response = getattr(model, "search")().from_dict(body).execute()
     hot_values = [i.key for i in response.aggregations.hot_counts.buckets]
