5a0618ad2cba995e307e8f02cc772503d64df765
Franziska Schloesser <schloesser@zib.de>
2017-08-31 11:07:49 +0200

    fix query selecting options for search

--- rubberband/utils/es_helpers.py
+++ rubberband/utils/es_helpers.py
@@ -1,10 +1,11 @@
+import logging
 
 def get_uniques(model, field):
     '''
     Take an elasticsearch model and a field name. The looks up all of the possible values
     for that field in elasticsearch.
     '''
-    body = {'aggs': {'counts': {'terms': {'field': field}}}, 'size': 0}
+    body = {'aggs': {'counts': {'terms': {'field': field, 'size': 0}}}}
     # this should be the same as 'model.search()'.from_dict(body).execute() ...
     response = getattr(model, "search")().from_dict(body).execute()
     values = [i.key for i in response.aggregations.counts.buckets]
