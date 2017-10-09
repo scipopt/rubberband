01d53dc7ce1bfa2cdcd1081c15848f139b8bc970
Franziska Schloesser <schloesser@zib.de>
2017-10-09 10:54:16 +0200

    enable more evalfiles

--- rubberband/constants.py
+++ rubberband/constants.py
@@ -6,8 +6,16 @@ FILES_DIR = "files/"
 STATIC_FILES_DIR = FILES_DIR + "static/"
 ALL_SOLU = STATIC_FILES_DIR + "all.solu"
 IPET_EVALUATIONS = {
-        0: {"path": STATIC_FILES_DIR + "eval.xml", "name": "standard evaluation"},
-        1: {"path": STATIC_FILES_DIR + "eval_seeds.xml", "name": "seeds evaluation"}
+        0: {"path": STATIC_FILES_DIR + "eval1.xml", "name": "evaluation1"},
+        1: {"path": STATIC_FILES_DIR + "eval2.xml", "name": "evaluation2"},
+        2: {"path": STATIC_FILES_DIR + "eval3.xml", "name": "evaluation3"},
+        3: {"path": STATIC_FILES_DIR + "eval4.xml", "name": "evaluation4"},
+        4: {"path": STATIC_FILES_DIR + "eval5.xml", "name": "evaluation5"},
+        5: {"path": STATIC_FILES_DIR + "eval6.xml", "name": "evaluation6"},
+        6: {"path": STATIC_FILES_DIR + "eval1.xml", "name": "evaluation7"},
+        7: {"path": STATIC_FILES_DIR + "eval2.xml", "name": "evaluation8"},
+        8: {"path": STATIC_FILES_DIR + "eval3.xml", "name": "evaluation9"},
+        9: {"path": STATIC_FILES_DIR + "eval4.xml", "name": "evaluation10"}
         }
 NONE_DISPLAY = "--"
 EXPORT_DATA_FORMATS = ("gzip", "json", "csv", "raw")
