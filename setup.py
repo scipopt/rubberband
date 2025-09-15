99452a087895dede242ebee2e3ae4967a801ebae
Cristina Muñoz <cristina@cradle.app>
2025-09-15 11:00:47 -0500

    Prune setup.py even more

--- setup.py
+++ setup.py
@@ -1,19 +1,3 @@
-#!/usr/bin/env python
 import setuptools
-from distutils.core import setup
 
-with open("requirements.txt") as requirements:
-    required = requirements.read().splitlines()
-
-kwargs = {
-    "packages": setuptools.find_packages(),
-    "install_requires": required[1:],
-    "classifiers": [
-        "Programming Language :: Python",
-        "Topic :: Software Development",
-        "Topic :: Software Development :: Libraries",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-}
-
-setup(**kwargs)
+setuptools.setup(packages=setuptools.find_packages())
